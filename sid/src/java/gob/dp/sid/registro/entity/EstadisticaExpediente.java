cbe289e3a13334e50b0ec278f96d5b0c75822df9
ccarrillo <ccarrillojara@gmail.com>
2018-12-16 19:10:34 -0500

    Modulo de Administracion Usuario Ventanilla

--- sid/src/java/gob/dp/sid/registro/entity/EstadisticaExpediente.java
+++ sid/src/java/gob/dp/sid/registro/entity/EstadisticaExpediente.java
@@ -5,9 +5,7 @@
  */
 package gob.dp.sid.registro.entity;
 
-import gob.dp.sid.atencion.entity.*;
 import java.io.Serializable;
-import java.util.Date;
 
 /**
  *
@@ -18,7 +16,12 @@ public class EstadisticaExpediente implements Serializable {
     private Integer cantQuejas;
     private Integer cantConsultas;
     private Integer cantPetitorios;
-
+    private Integer cantTramite;
+    private Integer cantConcluido;
+    private Integer cantDesistido;
+    private Integer cantEnBorrador;
+    private Integer cantEnRegistro;
+    private String codUsuario;
     /**
      * @return the cantQuejas
      */
@@ -59,6 +62,90 @@ public class EstadisticaExpediente implements Serializable {
      */
     public void setCantPetitorios(Integer cantPetitorios) {
         this.cantPetitorios = cantPetitorios;
+    }
+
+    /**
+     * @return the cantTramite
+     */
+    public Integer getCantTramite() {
+        return cantTramite;
+    }
+
+    /**
+     * @param cantTramite the cantTramite to set
+     */
+    public void setCantTramite(Integer cantTramite) {
+        this.cantTramite = cantTramite;
+    }
+
+    /**
+     * @return the cantConcluido
+     */
+    public Integer getCantConcluido() {
+        return cantConcluido;
+    }
+
+    /**
+     * @param cantConcluido the cantConcluido to set
+     */
+    public void setCantConcluido(Integer cantConcluido) {
+        this.cantConcluido = cantConcluido;
+    }
+
+    /**
+     * @return the cantDesistido
+     */
+    public Integer getCantDesistido() {
+        return cantDesistido;
+    }
+
+    /**
+     * @param cantDesistido the cantDesistido to set
+     */
+    public void setCantDesistido(Integer cantDesistido) {
+        this.cantDesistido = cantDesistido;
+    }
+
+    /**
+     * @return the codUsuario
+     */
+    public String getCodUsuario() {
+        return codUsuario;
+    }
+
+    /**
+     * @param codUsuario the codUsuario to set
+     */
+    public void setCodUsuario(String codUsuario) {
+        this.codUsuario = codUsuario;
+    }
+
+    /**
+     * @return the cantEnBorrador
+     */
+    public Integer getCantEnBorrador() {
+        return cantEnBorrador;
+    }
+
+    /**
+     * @param cantEnBorrador the cantEnBorrador to set
+     */
+    public void setCantEnBorrador(Integer cantEnBorrador) {
+        this.cantEnBorrador = cantEnBorrador;
+    }
+
+    /**
+     * @return the cantEnRegistro
+     */
+    public Integer getCantEnRegistro() {
+        return cantEnRegistro;
+    }
+
+    /**
+     * @param cantEnRegistro the cantEnRegistro to set
+     */
+    public void setCantEnRegistro(Integer cantEnRegistro) {
+        this.cantEnRegistro = cantEnRegistro;
     }
     
 }
