bd321b109cfa92e67d15fce61998aac2c6fa9e1b
ccarrillojara <ccarrillojara@192.168.1.227>
2019-01-04 03:00:15 -0500

    Incidencias

--- sid/src/java/gob/dp/sid/registro/entity/ExpedientePersona.java
+++ sid/src/java/gob/dp/sid/registro/entity/ExpedientePersona.java
@@ -50,6 +50,12 @@ public class ExpedientePersona implements Serializable{
     private Boolean indicadorDiscapacitado;
     
     private String tipoPueblo;
+    
+    private String nombre;
+    
+    private String apellidoPat;
+    
+    private String apellidoMat;
 
     public ExpedientePersona(Expediente expediente, Persona persona) {
         this.expediente = expediente;
@@ -211,5 +217,28 @@ public class ExpedientePersona implements Serializable{
         this.id = id;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getApellidoPat() {
+        return apellidoPat;
+    }
+
+    public void setApellidoPat(String apellidoPat) {
+        this.apellidoPat = apellidoPat;
+    }
+    
+    public String getApellidoMat() {
+        return apellidoMat;
+    }
+
+    public void setApellidoMat(String apellidoMat) {
+        this.apellidoMat = apellidoMat;
+    }
     
 }
