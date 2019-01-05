3aa69186682685159bc02c5968b064885b05e75b
ccarrillojara <ccarrillojara@192.168.1.43>
2019-01-05 18:51:23 -0500

    Incidencias - Guardar Oficio

--- sid/src/java/gob/dp/sid/registro/controller/RegistroController.java
+++ sid/src/java/gob/dp/sid/registro/controller/RegistroController.java
@@ -4481,8 +4481,10 @@ public class RegistroController extends AbstractManagedBean implements Serializa
                 expediente.setUsuarioResponsable(usuarioSession.getCodigo());
                 expediente.setVersion(1);
                 generarCodigoExpediente();
-                documento  = personasSeleccionadas.get(0).getPersona().getNumeroDocumento();
-                validaEnvioEmail = expedienteService.validaUsuarioCount(documento);
+                if(personasSeleccionadas != null && personasSeleccionadas.size()>0){
+                    documento  = personasSeleccionadas.get(0).getPersona().getNumeroDocumento();
+                    validaEnvioEmail = expedienteService.validaUsuarioCount(documento);    
+                }
                 expediente.setFechaIngreso(new Date());
                 expediente.setFechaRegistro(new Date());
             } else {
