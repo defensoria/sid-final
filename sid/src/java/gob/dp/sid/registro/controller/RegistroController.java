4ef8a787c63d9245e3847bcbe66bfcb5a6a8883a
ccarrillojara <ccarrillojara@192.168.1.43>
2019-01-05 19:00:31 -0500

    Incidencias - Para queja cargar por defecto Recurrente y Afectado

--- sid/src/java/gob/dp/sid/registro/controller/RegistroController.java
+++ sid/src/java/gob/dp/sid/registro/controller/RegistroController.java
@@ -578,6 +578,9 @@ public class RegistroController extends AbstractManagedBean implements Serializa
             expediente.setTipoClasificion(personaSeleccionada.getTipoExpediente());
             ep.setPersona(personaSeleccionada);
             personasSeleccionadas.add(ep);
+            if(personasSeleccionadas.size()==1 && personaSeleccionada.getTipoExpediente().equals(ExpedienteType.QUEJA.getKey())){
+                personasSeleccionadas.get(0).setTipo("02");
+            }
             inicializarEtapaEstado(0);
             setVerBotonRegistrarExpediente(true);
             expedienteClasificacionBusqueda = new ExpedienteClasificacion();
